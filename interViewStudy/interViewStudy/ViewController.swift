6021c07800863e60681cc9c7b9e34f4a424dde39
jgkim1008 <rlawnsrjs100@naver.com>
2022-02-15 03:42:32 +0900

    chore: 초기 셋팅
    * CodeBase UI를 하기 위하여 SceneDelegate 설정완료

---
+++ interViewStudy/interViewStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  interViewStudy
+//
+//  Created by 김준건 on 2022/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
