26246ce8dd8b81e98fc31cbe3a4ccdf2b535aa8f
jgkim1008 <rlawnsrjs100@naver.com>
2022-02-28 23:27:18 +0900

    feat: MainViewModel 타입구현
    * 메인 뷰컨에서 보여줘야할 데이터를 가공하는 ViewModel 구현

--- interViewStudy/interViewStudy/ViewModel/MainViewModel.swift
+++ interViewStudy/interViewStudy/ViewModel/MainViewModel.swift
@@ -6,30 +6,60 @@
 //
 
 import Foundation
+import UIKit
 
-class MainViewModel {
+enum viewState {
+    case idle
+    case isLoading
+}
+
+enum Section {
+    case main
+}
+typealias DataSource = UICollectionViewDiffableDataSource<Section, MovieInfoModel>
+typealias SnapShot = NSDiffableDataSourceSnapshot<Section, MovieInfoModel>
+
+final class MainViewModel {
     private let networkManager = NetworkManager(networkable: NetworkModule())
-    private var movieData: [Movie] = [] {
-        didSet {
-            NotificationCenter.default.post(name: .dataReceiveNotifiaction, object: nil)
-        }
-    }
+    private let imageManager = ImageManager()
+    var currentPage = 1
+    var dataSource: DataSource?
+    var viewState: viewState = .idle
+    var movieList: [MovieInfoModel] = []
     
-    init() {
-        fetchData(pageNum: "1")
-    }
-    
-    func fetchData(pageNum: String) {
+    func fetchData() {
+        guard viewState == .idle else { return }
         let route = TMDBRoute.nowPlaying
+        self.viewState = .isLoading
+        
         networkManager.request(with: route,
-                               queryItems: route.generateNowPlayingQueryItems(apiKey: "1"),
-                               requestType: .request) { [weak self](result: Result<Movie, Error>) in
+                               queryItems: route.generateNowPlayingQueryItems(page: currentPage),
+                               requestType: .request) { [weak self](result: Result<MovieModel, Error>) in
             switch result {
             case .success(let data):
-                self?.movieData.append(data)
+                self?.currentPage += 1
+                
+                data.result.forEach { data in
+                    guard let url = URL(string: "https://image.tmdb.org/t/p/w500/" + (data.posterPath ?? "")) else { return }
+                    self?.imageManager.getImage(with: url) { result in
+                        guard case .success(let imageData) = result else { return }
+                        self?.movieList.append(MovieInfoModel(title: data.title, voteAverage: data.voteAverage.description, uuid: UUID(), image: imageData))
+                        self?.viewState = .idle
+                    }
+                }
+                var snapShot = SnapShot()
+                if snapShot.sectionIdentifiers.isEmpty {
+                    snapShot.appendSections([.main])
+                }
+                guard let movieList = self?.movieList else { return }
+                snapShot.appendItems(movieList)
+                self?.dataSource?.apply(snapShot, animatingDifferences: false)
+
             case .failure(let error):
-                print(error)
+                assertionFailure(error.localizedDescription)
             }
         }
     }
+    
+  
 }
