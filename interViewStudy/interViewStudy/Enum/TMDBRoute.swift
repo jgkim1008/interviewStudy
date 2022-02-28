1b89c9a3e54b96f697272ebe26a5fe3575953edc
jgkim1008 <rlawnsrjs100@naver.com>
2022-02-28 23:23:22 +0900

    feat: generateSearchQueryItems 함수 구현
    * search에 필요한 쿼리 아이템을 만들어주는 함수 구현

--- interViewStudy/interViewStudy/Enum/TMDBRoute.swift
+++ interViewStudy/interViewStudy/Enum/TMDBRoute.swift
@@ -9,6 +9,8 @@ import Foundation
 
 enum TMDBRoute: Route {
     case nowPlaying
+    case getImage
+    case search
     
     var scheme: String {
         return "https"
@@ -22,14 +24,25 @@ enum TMDBRoute: Route {
         switch self {
         case .nowPlaying:
             return "/3/movie/now_playing"
+        case .getImage:
+            return "image.tmdb.org/t/p/w500/"
+        case .search:
+            return "/3/search/movie"
         }
     }
     
-    func generateNowPlayingQueryItems(apiKey: String, page: String = "1") -> [URLQueryItem] {
+    func generateNowPlayingQueryItems(page: Int = 1) -> [URLQueryItem] {
         var queryItems: [URLQueryItem] = []
         queryItems.append(URLQueryItem(name: "api_key",value:"6a57b97030d7523e4a313c9dbd106cdd"))
         queryItems.append(URLQueryItem(name: "language", value: "en-US"))
-        queryItems.append(URLQueryItem(name: "page", value: page))
+        queryItems.append(URLQueryItem(name: "page", value: page.description))
+        return queryItems
+    }
+    
+    func generateSearchQueryItems(query: String?) -> [URLQueryItem] {
+        var queryItems: [URLQueryItem] = []
+        queryItems.append(URLQueryItem(name: "query", value: query))
+        queryItems.append(URLQueryItem(name: "api_key",value:"6a57b97030d7523e4a313c9dbd106cdd"))
         return queryItems
     }
 }
